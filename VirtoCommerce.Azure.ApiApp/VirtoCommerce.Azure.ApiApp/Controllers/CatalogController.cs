a61a382ef187e04fa9abbc5e53a3212f96fcbece
aartiomov <andrej.artiomov@gmail.com>
2015-10-15 18:12:50 +0300

    - Initial VirtoCommerce API App version.

---
+++ VirtoCommerce.Azure.ApiApp/VirtoCommerce.Azure.ApiApp/Controllers/CatalogController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using VirtoCommerce.SwaggerApiClient;
+using VirtoCommerce.SwaggerApiClient.Api;
+using VirtoCommerce.SwaggerApiClient.Client;
+using VirtoCommerce.SwaggerApiClient.Model;
+
+namespace VirtoCommerce.Azure.ApiApp.Controllers
+{
+    [RoutePrefix("api/catalog/catalogs")]
+    public class CatalogController : ApiController
+    {        
+        /// <summary>
+        /// Get Catalogs list
+        /// </summary>
+        /// <remarks>Get common and virtual Catalogs list with minimal information included. Returns array of Catalog</remarks>
+		[HttpGet]        
+        [Route("")]
+        public IEnumerable<VirtoCommerceCatalogModuleWebModelCatalog> GetAll(string basePath, string appId, string key)
+        {
+            var apiClient = new HmacApiClient(basePath, appId, key);
+            var catalogClient = new CatalogModuleApi(apiClient);
+            return catalogClient.CatalogModuleCatalogsGetCatalogs();
+        }
+
+        /// <summary>
+        /// Gets Catalog by id.
+        /// </summary>
+        /// <remarks>Gets Catalog by id with full information loaded</remarks>
+        /// <param name="id">The Catalog id.</param>
+        [HttpGet]
+        [Route("{id}")]
+        public VirtoCommerceCatalogModuleWebModelCatalog Get(string basePath, string appId, string key, string catalogId)
+        {
+            var apiClient = new HmacApiClient(basePath, appId, key);
+            var catalogClient = new CatalogModuleApi(apiClient);
+            return catalogClient.CatalogModuleCatalogsGet(catalogId);
+        }
+
+        /// <summary>
+        /// Updates the specified catalog.
+        /// </summary>
+        /// <remarks>Updates the specified catalog.</remarks>
+        /// <param name="catalog">The catalog.</param>
+        [HttpPut]
+        [ResponseType(typeof(void))]
+        [Route("")]
+        public IHttpActionResult Update(string basePath, string appId, string key, VirtoCommerceCatalogModuleWebModelCatalog catalog)
+        {
+            var apiClient = new HmacApiClient(basePath, appId, key);
+            var catalogClient = new CatalogModuleApi(apiClient);
+            UpdateCatalog(catalogClient, catalog);
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+        
+        private void UpdateCatalog(CatalogModuleApi apiClient, VirtoCommerceCatalogModuleWebModelCatalog catalog)
+        {
+            apiClient.CatalogModuleCatalogsUpdate(catalog);
+        }
+
+
+    }
+}
---
+++ VirtoCommerce.Azure.ApiApp/VirtoCommerce.Azure.ApiApp/Controllers/ProductController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using VirtoCommerce.SwaggerApiClient;
+using VirtoCommerce.SwaggerApiClient.Api;
+using VirtoCommerce.SwaggerApiClient.Client;
+using VirtoCommerce.SwaggerApiClient.Model;
+
+namespace VirtoCommerce.Azure.ApiApp.Controllers
+{
+    [RoutePrefix("api/catalog/products")]
+    public class ProductController : ApiController
+    {        
+        /// <summary>
+        /// Get Product by Id
+        /// </summary>
+        /// <remarks>Get product by provided id</remarks>
+		[HttpGet]        
+        [Route("")]
+        public VirtoCommerceCatalogModuleWebModelProduct GetById(string basePath, string appId, string key, string productId)
+        {
+            var apiClient = new HmacApiClient(basePath, appId, key);
+            var catalogClient = new CatalogModuleApi(apiClient);
+            return catalogClient.CatalogModuleProductsGet(productId);
+        }
+
+        /// <summary>
+        /// Updates the specified product.
+        /// </summary>
+        /// <remarks>Updates the specified product.</remarks>
+        /// <param name="product">The product.</param>
+        [HttpPut]
+        [ResponseType(typeof(void))]
+        [Route("")]
+        public IHttpActionResult Update(string basePath, string appId, string key, VirtoCommerceCatalogModuleWebModelProduct product)
+        {
+            var apiClient = new HmacApiClient(basePath, appId, key);
+            var catalogClient = new CatalogModuleApi(apiClient);
+            UpdateProduct(catalogClient, product);
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+        
+        private void UpdateProduct(CatalogModuleApi apiClient, VirtoCommerceCatalogModuleWebModelProduct product)
+        {
+            apiClient.CatalogModuleProductsUpdate(product);
+        }
+
+
+    }
+}
